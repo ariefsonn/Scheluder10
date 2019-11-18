4c98348a5ee104fe59bc39fee578cd387fb9e056
royyan1207070 <royyan120707@gmail.com>
2019-11-18 11:17:45 +0700

    alpha 0.3

--- component/Menja/index.js
+++ component/Menja/index.js
@@ -26,12 +26,16 @@ export default class Menja extends Component {
       <View style={{ padding: 32 }}>
         <Image source={require('../../assets/schw.png')} style={{ width: 200, height: 180, alignSelf: 'center', marginTop: 10 }} />
         <View style={{ flexDirection: 'row', marginTop: 20 }}>
+
           <TextInput keyboardType="default" placeholder="the name of the schedule" style={{ padding: 12, borderWidth: 2, borderRadius: 4, borderColor: 'purple', width: 250 }} />
-          <TouchableOpacity style={{ padding: 14, backgroundColor: 'purple', alignItems: 'center', marginRight: 20, marginLeft: 20 }}>
-              <Image source={require('../../assets/tambah.png')} style={{ width: 20, height: 20 }} />
+
+          <TouchableOpacity style={{ padding: 14, backgroundColor: 'purple', alignItems: 'center', marginRight: 20,   marginLeft: 20 }}>
+            
+              <Image source={require('../../assets/tambah.png')} style={{ width: 15, height: 15 }} />
+
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={{ padding: 10, backgroundColor: 'purple', alignItems: 'center', marginRight: 80,marginLeft: 80, marginTop: 30 }}>
+        <TouchableOpacity style={{ borderRadius: 20, padding: 10, backgroundColor: 'purple', alignItems: 'center', marginRight: 80,marginLeft: 80, marginTop: 30 }}>
           <Text style={{ fontSize: 10, color: 'white' }}>Buat Jadwal</Text>
         </TouchableOpacity>
       </View>
--- component/Staja/index.js
+++ component/Staja/index.js
@@ -1,12 +1,3 @@
 import React, { Component } from 'react'
 import { Text, View, TextInput, TouchableOpacity, FlatList, ScrollView, Image } from 'react-native'
 
-export default class Staja extends Component {
-  render(){
-    return(
-      <TouchableOpacity style={{ padding: 12, backgroundColor: 'purple', alignItems: 'center', marginRight: 170, marginLeft: 170, marginTop: 500 }}>
-        <Image source={require('../../assets/tambah.png')} style={{ width: 20, height: 20 }} />
-      </TouchableOpacity>
-    )
-  }
-} 
